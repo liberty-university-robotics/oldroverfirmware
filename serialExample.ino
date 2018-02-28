b89557a56beba44ffd20747380d2f914a7e7d1af
team pi <pi@raspberry.com>
2018-02-28 01:59:35 +0000

    add code adjusted to left side or right side logic for two arduino as controls

--- serialExample.ino
+++ serialExample.ino
@@ -1,12 +1,21 @@
-byte incomingByte = 0;	// for incoming serial data
+#include "DualVNH5019MotorShield.h"
 
 DualVNH5019MotorShield md;
+
+byte incomingByte = 0;	// for incoming serial data
+
 
 
 #ifndef FORWARD
 #define FORWARD true
 #endif
 
+//If motor side is true then it is the right side, false is left
+#ifndef MOTOR_SIDE
+#define MOTOR_SIDE false
+#endif
+
+ 
 
 #ifndef BACKWARD
 #define BACKWARD false
@@ -27,34 +36,111 @@ void stopIfFault()
 }
 
 void setup() {
+	md.init();
 	Serial.begin(9600);	// opens serial port, sets data rate to 9600 bps
 }
 
 bool name = FORWARD;
 
-
 void loop() {
-	/*if (Serial.available()) {
-		incomingByte = Serial.read();
-         	if(incomingByte == 'w'){
-			setSpeeds(200,200);
-         	}else if(incomingByte == 'a'){
-			setSpeeds(0,200);
-		}else if(incomingByte == 'd'){
-			setSpeeds(200,0);
-		}else if(incomingByte == 's'){
-			setSpeeds(-200,-200);
-		}else if(incomingByte == 'b'){
-			setBrakes(400,400);
-		}else if(incomingByte == 'c'){
-			setBrakes(0,0);
+	if(MOTOR_SIDE){
+		while(1){
+			if (Serial.available()) {
+				incomingByte = Serial.read();
+                		if(incomingByte == 'w'){
+                        		name = FORWARD;
+                	        	md.setSpeeds(200,200);
+                		}else if(incomingByte == 's'){
+                        		name = BACKWARD;
+                        		md.setSpeeds(-200,-200);
+                		}else if(incomingByte == 'b'){
+                        		md.setBrakes(400,400);
+                		}else if(incomingByte == 'c'){
+                        		md.setBrakes(0,0);
+                		}   
+                		if(name == FORWARD){
+                 		       if(incomingByte == 'a'){
+                                		md.setSpeeds(200,200);
+                        		}else if(incomingByte == 'd'){
+                                		md.setSpeeds(0,0);
+                        		}
+				}else{
+					if(incomingByte == 'a'){
+                       			         md.setSpeeds(-200,-200);
+                        		}else if(incomingByte == 'd'){
+                                		md.setSpeeds(0,0);
+                        		}
+				}  
+                        		
+			}
+
 		}
-	}*/
+	}else{
+		while(1){
+                        if (Serial.available()) {
+                                incomingByte = Serial.read();
+                                if(incomingByte == 'w'){
+                        		name = FORWARD;
+                                        md.setSpeeds(200,200);
+                                }else if(incomingByte == 's'){
+                                        name = BACKWARD;
+                                        md.setSpeeds(-200,-200);
+                                }else if(incomingByte == 'b'){
+                                        md.setBrakes(400,400);
+                                }else if(incomingByte == 'c'){
+                                        md.setBrakes(0,0);
+                                }
+                                if(name == FORWARD){
+                                       if(incomingByte == 'a'){
+                                                md.setSpeeds(0,0);
+                                        }else if(incomingByte == 'd'){
+                                                md.setSpeeds(200,200);
+                                        }
+                                }else{  
+                                        if(incomingByte == 'a'){
+                                                 md.setSpeeds(0,0);
+                                        }else if(incomingByte == 'd'){
+                                                md.setSpeeds(-200,-200);
+                                        }
+                                }       
+                                        
+                        }
+
+                }
+	}
+}/*
 	if (Serial.available()) {
                 incomingByte = Serial.read();
                 if(incomingByte == 'w'){
                         setSpeeds(200,200);
                 }else if(incomingByte == 's'){
+                        name = BACKWORD;
+                        setSpeeds(-200,-200);
+                }else if(incomingByte == 'b'){
+                        setBrakes(400,400);
+                }else if(incomingByte == 'c'){
+                        setBrakes(0,0);
+                }
+                if(name == FORWARD){
+                        if(incomingByte == 'a'){
+                                setSpeeds(0,200);
+                        }else if(incomingByte == 'd'){
+                                setSpeeds(200,0);
+                        }   
+                }else{
+                        if(incomingByte == 'a'){
+                                setSpeeds(0,-200);
+                        }else if(incomingByte == 'd'){
+                                setSpeeds(-200,0);
+                        }
+                }
+        }
+	/*if (Serial.available()) {
+                incomingByte = Serial.read();
+                if(incomingByte == 'w'){
+                        setSpeeds(200,200);
+                }else if(incomingByte == 's'){
+			name = BACKWORD;
                         setSpeeds(-200,-200);
                 }else if(incomingByte == 'b'){
                         setBrakes(400,400);
@@ -74,6 +160,6 @@ void loop() {
                                 setSpeeds(-200,0);
                         }
                 }
-        }
+        }*/
 
-}
+
